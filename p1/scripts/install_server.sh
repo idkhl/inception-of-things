9798833d25cbf9f89716e0097ad74f3602dea228
idkhl <ines.dakhlaoui@gmail.com>
2026-04-26 19:36:11 +0200

    vagrantfile + srcipts installation k3s

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="idakhlaoS" sh -s - --flannel-backend none --token 12345
+curl -sfL https://get.k3s.io | K3S_TOKEN="12345" INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110" sh -
--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="idakhlaoSW" sh -s - --flannel-backend none --token 12345
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="12345" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
