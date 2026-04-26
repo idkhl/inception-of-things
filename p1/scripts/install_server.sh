3188c5e353be0e2857d60e5100308fe22599c467
cyberintrusion <126964670+cyberintrusion@users.noreply.github.com>
2026-04-26 23:04:38 +0200

    on m'a force

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+apt update -y
+apt-get install -y curl
+
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110" sh -
 
 sleep 5
