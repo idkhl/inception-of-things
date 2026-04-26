38dd154285aa8a8f2a1342bc8d0cf5a2cf21469e
cyberintrusion <126964670+cyberintrusion@users.noreply.github.com>
2026-04-26 22:06:06 +0200

    Update install_server.sh for K3S installation
    Remove K3S_TOKEN from installation command and add sleep and token retrieval steps.

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-curl -sfL https://get.k3s.io | K3S_TOKEN="12345" INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110" sh -
+
+sleep 5
+
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
