57bfc2b4f566e60e4c2d8191880c6f4cf95daff3
cyberintrusion <126964670+cyberintrusion@users.noreply.github.com>
2026-04-26 22:06:49 +0200

    Use NODE_TOKEN from file for K3S installation

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="12345" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
+
+NODE_TOKEN=$(cat /vagrant/node-token)
+
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${NODE_TOKEN}" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
