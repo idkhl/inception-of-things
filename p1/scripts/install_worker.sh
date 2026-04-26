4108ed844fcd042e6a26c652cad4117ce079c55f
cyberintrusion <126964670+cyberintrusion@users.noreply.github.com>
2026-04-26 23:04:55 +0200

    a l'aide

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+apt update -y
+
 NODE_TOKEN=$(cat /vagrant/node-token)
 
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${NODE_TOKEN}" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
