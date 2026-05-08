ac7ee7f9b455c166f1a1995c94eccd27450cb28f
idakhlao <idakhlao@idakhlao.idakhlao>
2026-05-08 19:05:37 +0100

    update scripts p1

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -3,8 +3,12 @@
 apt update -y
 apt-get install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110" sh -
+curl -sfL https://get.k3s.io | sh -s - server \
+  --write-kubeconfig-mode 644 \
+  --node-ip 192.168.56.110 \
+  --bind-address 192.168.56.110 \
+  --flannel-iface eth1
 
-sleep 5
+sleep 15
 
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
