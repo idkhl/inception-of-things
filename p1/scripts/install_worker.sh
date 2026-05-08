b953ae0648ba3463833ced15ed276205c642542a
idkhl <ines.dakhlaoui@gmail.com>
2026-05-08 20:14:04 +0200

    script p1

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-apt update -y
+# apt update -y
+
+# NODE_TOKEN=$(cat /vagrant/node-token)
+
+# curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${NODE_TOKEN}" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
+
+
+apt-get update -y
+apt-get install -y curl
 
 NODE_TOKEN=$(cat /vagrant/node-token)
 
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${NODE_TOKEN}" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="${NODE_TOKEN}" sh -s - agent \
+  --node-ip 192.168.56.111 \
+  --flannel-iface eth1
